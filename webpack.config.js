b9c09a2ce70aed10457816ffaaea4a6848975300
Letha <32145474+Letha@users.noreply.github.com>
2019-03-26 20:57:50 +0000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+﻿const path = require('path');
+
+module.exports = {
+
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  output: {
+
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      },
+
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
